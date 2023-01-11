6af6929ac7941643070dbbda806469c9098b0cba
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2023-01-11 16:13:13 -0300

    :white_check_mark: Adicionando novos testes trabalhados na Seção 7 do curso

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -107,4 +107,31 @@ describe('Central de atendimento ao Cliente TAT', () => {
       .should('not.be.checked')
   })
 
+  it('Seleciona um arquivo da pasta fixtures', () => {
+    cy.get('input[type="file"]#file-upload')
+      .should('not.have.value')
+      .selectFile('./cypress/fixtures/example.json')
+      .should(function($input) {
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+  })
+
+  it('Seleciona um arquivo simulando um drag-and-drop', () => {
+    cy.get('input[type="file"]#file-upload')
+      .should('not.have.value')
+      .selectFile('./cypress/fixtures/example.json', {action: 'drag-drop'})
+      .should(function($input) {
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+  })
+
+  it('Seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+    cy.fixture("example.json").as('sampleFile')
+    cy.get('input[type="file"]#file-upload')
+      .selectFile('@sampleFile')
+      .should(function($input) {
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+  })
+
 })
