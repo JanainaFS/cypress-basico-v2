de6a84681e23f88b9b998d1562d646593b641d30
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2023-01-31 15:29:35 -0300

    :white_check_mark: Adicionando caso de teste utilizando cy.request

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -176,12 +176,22 @@ describe('Central de atendimento ao Cliente TAT', () => {
       .should('not.be.visible')
   })
 
-  it.only('Preenche a area de texto usando o comando invoke', () => {
+  it('Preenche a area de texto usando o comando invoke', () => {
     const longText = Cypress._.repeat('0123456789', 20)
 
     cy.get('#open-text-area')
       .invoke('val', longText)
       .should('have.value', longText)
+  })
+
+  it('Faz uma requisição HTTP', () => {
+    cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+      .should(function(response) {
+        const {status, statusText, body} = response
+        expect(status).to.equal(200)
+        expect(statusText).to.equal('OK')
+        expect(body).to.include('CAC TAT')
+      })
   })
 
 })
